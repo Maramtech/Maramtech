22f7a0e35f2222e26b2171fc980881fbd1fd8e1f
Maramtech <107195709+Maramtech@users.noreply.github.com>
2022-06-16 17:52:28 +0530

    KDOKS

--- js/header.js
+++ js/header.js
@@ -1,10 +1,1 @@
-$(window).scroll(function(){
-    if ($(window).scrollTop() >= 300) {
-        $('header').addClass('fixed-header');
-        $('header div').addClass('visible-title');
-    }
-    else {
-        $('header').removeClass('fixed-header');
-        $('header div').removeClass('visible-title');
-    }
-});
+
