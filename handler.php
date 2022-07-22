886027a50c513eaf53541bd8bb8d4ede32740331
Maramtech <107195709+Maramtech@users.noreply.github.com>
2022-07-22 15:59:41 +0530

    edfwa

---
+++ handler.php
@@ -0,0 +1,37 @@
+<?php
+
+if(isset($_POST['submit'])) {
+ $mailto = "nizamuddinalthaf@gmail.com";  //My email address
+ //getting customer data
+ $name = $_POST['name']; //getting customer name
+ $fromEmail = $_POST['email']; //getting customer email
+ $phone = $_POST['tel']; //getting customer Phome number
+ $subject = $_POST['subject']; //getting subject line from client
+ $subject2 = "Confirmation: Message was submitted successfully"; // For customer confirmation
+
+ //Email body I will receive
+ $message = "Cleint Name: " . $name . "\n"
+ . "Phone Number: " . $email . "\n\n"
+ . "Client Message: " . "\n" . $_POST['message'];
+
+ //Message for client confirmation
+ $message2 = "Dear" . $name . "\n"
+ . "Thank you for contacting us. We will get back to you shortly!" . "\n\n"
+ . "You submitted the following message: " . "\n" . $_POST['message'] . "\n\n"
+ . "Regards," . "\n" . "- HMA WebDesign";
+
+ //Email headers
+ $headers = "From: " . $fromEmail; // Client email, I will receive
+ $headers2 = "From: " . $mailto; // This will receive client
+
+ //PHP mailer function
+
+  $result1 = mail($mailto, $subject, $message, $headers); // This email sent to My address
+  $result2 = mail($fromEmail, $subject2, $message2, $headers2); //This confirmation email to client
+
+  //Checking if Mails sent successfully
+
+
+}
+
+?>
